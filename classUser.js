39429ee0276acd2a5ff477079918d1ed63a28e91
thaleseqb <thalesquadros2@gmail.com>
2024-03-01 13:29:26 -0300

    instancing the class (User) object and verifying if it was instanced correctly

--- classUser.js
+++ classUser.js
@@ -6,4 +6,18 @@ class User {
         this.role = role || 'student';
         this.active = active;        
     };
+
+    exhibitInfo() {
+        return `the student's name and email are ${this.name}, ${this.email}`;
+    }
+
+
 };
+
+// the command new is used to invoke the cosntructor function
+// and the class object is returned
+const newUser = new User('thales', 't@t.com', '20')
+
+console.log(newUser.exhibitInfo())
+// the command below shows if User is prototype of newUser
+console.log(User.prototype.isPrototypeOf(newUser))
