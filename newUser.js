6c4ecffd8a94f95971192ad4ca89a81b89eb3c86
thaleseqb <thalesquadros2@gmail.com>
2024-03-01 10:56:49 -0300

    adding some

--- newUser.js
+++ newUser.js
@@ -2,13 +2,13 @@
 
 // constructor function
 function User(name, email) {
-    this.name = name
-    this.email = email
+    this.name = name;
+    this.email = email;
 
     this.exhibitInfo = function(){
-        return `${this.name}, ${this.email}` 
-    }
-}
+        return `${this.name}, ${this.email}`;
+    };
+};
 
 // const newUser = new User('Mr. java', 'j@j.com')
 // console.log(newUser.exhibitInfo())
@@ -17,27 +17,27 @@ function User(name, email) {
 function Admin(role) {
     // The call method is geting the properties definedin User
     // Now the context of User belongs to Admin too.
-    User.call(this, 'Mr. java', 'j@j.com')
-    this.role = role || 'estudante'
+    User.call(this, 'Mr. java', 'j@j.com');
+    this.role = role || 'estudante';
 }
 
-Admin.prototype = Object.create(User.prototype)
-// const newUser = new Admin('admin')
+Admin.prototype = Object.create(User.prototype);
+// const newUser = new Admin('admin');
 
-// console.log(newUser.exhibitInfo())
-// console.log(newUser.role)
+// console.log(newUser.exhibitInfo());
+// console.log(newUser.role);
 
 const user = {
     init: function(name, email) {
-        this.name = name
-        this.email = email        
+        this.name = name;
+        this.email = email;        
     },
     exhibitInfo: function(){
-        return this.name
+        return this.name;
     }
 }
 
-const newUser = Object.create(user)
-newUser.init('thales', 't@t.com')
-// console.log(newUser.exhibitInfo())
+const newUser = Object.create(user);
+newUser.init('thales', 't@t.com');
+// console.log(newUser.exhibitInfo());
 
